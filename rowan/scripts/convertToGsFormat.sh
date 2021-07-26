cc443939c573e3e0f06dd9a9e6403f8c4b2dd624
Andrei Chis <chisvasileandrei@gmail.com>
2021-07-26 10:47:27 +0200

    update rowan scripts

---
+++ rowan/scripts/convertToGsFormat.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+### Install gtoolkit-gemstone from Tonel files into a Rowan-enabled stone
+### Exits with 0 if success, 1 if failed
+
+gt4GemstoneHome=${ROWAN_PROJECTS_HOME}/gtoolkit-gemstone
+## Topaz refuses to exit from script if input is stdin, so redirect from /dev/zero
+topaz -l -I ${gt4GemstoneHome}/rowan/scripts/loginSystemUser.topaz  -S ${gt4GemstoneHome}/rowan/scripts/convertToGsFormat.topaz < /dev/zero
+if [ $? = 0 ]
+    then
+        exit 0
+    else
+        echo !!!!!!!!!!!!!!
+        echo CONVERTION FAILED
+        echo !!!!!!!!!!!!!!
+        exit 1
+    fi
