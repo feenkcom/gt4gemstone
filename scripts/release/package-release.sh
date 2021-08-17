b79ef66fa8e18d15ee536d6a8c1c408a1eb3016a
Andrei Chiș <chisvasileandrei@gmail.com>
2021-08-17 15:05:04 +0200

    refactor buuild scripts to use GEMSTONE_WORKSPACE

---
+++ scripts/release/package-release.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+set -e
+
+if [ -z "$ROWAN_PROJECTS_HOME" ]
+then
+	echo "ROWAN_PROJECTS_HOME must be defined"
+	exit 1
+fi
+
+if [ -z "$GEMSTONE_WORKSPACE" ]
+then
+	echo "GEMSTONE_WORKSPACE must be defined"
+	exit 1
+fi
+
+if [ -z "$GT4GEMSTONE_VERSION" ]
+then
+	echo "GT4GEMSTONE_VERSION must be defined"
+	exit 1
+fi
+
+if [ -z "$RELEASED_PACKAGE_GEMSTONE_NAME" ]
+then
+	echo "RELEASED_PACKAGE_GEMSTONE_NAME must be defined"
+	exit 1
+fi
+
+GT4GEMSTONE_RELEASE_FOLDER="${GEMSTONE_WORKSPACE}/${RELEASED_PACKAGE_GEMSTONE_NAME}"
+echo "GT4GEMSTONE_RELEASE_FOLDER=${GT4GEMSTONE_RELEASE_FOLDER}"
+if [ -e $GT4GEMSTONE_RELEASE_FOLDER ]
+then
+    echo "ERROR: release directory subdirectory already exists"
+    exit 1
+fi
+
+#Package the required files into an archive
+echo "Package release"
+
+SPARKLE_HOME=${ROWAN_PROJECTS_HOME}/Sparkle
+GT4GEMSTONE_HOME=${ROWAN_PROJECTS_HOME}/gt4gemstone
+GTOOLKIT_REMOTE_HOME=${ROWAN_PROJECTS_HOME}/gtoolkit-remote
+
+mkdir $GT4GEMSTONE_RELEASE_FOLDER
+mkdir -p ${GT4GEMSTONE_RELEASE_FOLDER}/Sparkle/src-gs
+mkdir -p ${GT4GEMSTONE_RELEASE_FOLDER}/gt4gemstone/src-gs
+mkdir -p ${GT4GEMSTONE_RELEASE_FOLDER}/gtoolkit-remote/src-gs
+
+cp ${SPARKLE_HOME}/src-gs/bootstrapSparkle.gs ${GT4GEMSTONE_RELEASE_FOLDER}/Sparkle/src-gs/
+cp ${GT4GEMSTONE_HOME}/src-gs/gt4gemstone.gs ${GT4GEMSTONE_RELEASE_FOLDER}/gt4gemstone/src-gs/
+cp ${GTOOLKIT_REMOTE_HOME}/src-gs/gtoolkit-remote.gs ${GT4GEMSTONE_RELEASE_FOLDER}/gtoolkit-remote/src-gs/
+
+cp ${GT4GEMSTONE_HOME}/scripts/release/inputRelease.sh ${GT4GEMSTONE_RELEASE_FOLDER}/
+cp ${GT4GEMSTONE_HOME}/scripts/release/inputRelease.topaz ${GT4GEMSTONE_RELEASE_FOLDER}/
+cp ${GT4GEMSTONE_HOME}/scripts/loginSystemUser.topaz ${GT4GEMSTONE_RELEASE_FOLDER}/
+
+zip ${GT4GEMSTONE_RELEASE_FOLDER}.zip $GT4GEMSTONE_RELEASE_FOLDER
+
+exit 0
--- scripts/setup-remote-gemstone.sh
+++ scripts/setup-remote-gemstone.sh
@@ -19,9 +19,15 @@ function stop_servers()
 
 export SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-if [ -e remote-gemstone ]
+if [ -z "$GEMSTONE_WORKSPACE" ]
 then
-	echo "ERROR: remote-gemstone subdirectory already exists"
+	echo "GEMSTONE_WORKSPACE must be defined"
+	exit 1
+fi
+
+if [ -e ${GEMSTONE_WORKSPACE} ]
+then
+	echo "ERROR: ${GEMSTONE_WORKSPACE} subdirectory already exists"
 	exit 1
 fi
 
@@ -36,22 +42,22 @@ then
 	wget http://downloads.gemtalksystems.com/pub/GemStone64/3.7.0-Alpha2/GemStoneClientLibs3.7.0-x86_64.Linux.zip
 fi
 
-mkdir remote-gemstone
-mkdir remote-gemstone/opt
-cd remote-gemstone
+# Set up environment variables
+source $SCRIPT_DIR/remote-gemstone-env.sh
+
+mkdir ${GEMSTONE_WORKSPACE}
+mkdir ${GEMSTONE_WORKSPACE}/opt
+cd ${GEMSTONE_WORKSPACE}
 unzip ../GemStone64Bit3.7.0-x86_64.Linux.zip
 unzip ../GemStoneClientLibs3.7.0-x86_64.Linux.zip
 
-mkdir rowan-projects
-cd rowan-projects
+mkdir ${ROWAN_PROJECTS_HOME}
+cd ${ROWAN_PROJECTS_HOME}
 ln -s ../../pharo-local/iceberg/feenkcom/gt4gemstone
 ln -s ../../pharo-local/iceberg/feenkcom/gtoolkit-remote
 git clone https://github.com/feenkcom/Sparkle.git
 cd ..
 cd ..
-
-# Set up environment variables
-source $SCRIPT_DIR/remote-gemstone-env.sh
 
 # Configure GemStone
 cd $GEMSTONE/install
@@ -71,9 +77,9 @@ ls -l $ROWAN_PROJECTS_HOME/gtoolkit-remote/scripts
 #ROWAN_PROJECTS_HOME/gt4gemstone/scripts/inputGt4gemstone.sh
 #ROWAN_PROJECTS_HOME/gtoolkit-remote/scripts/inputGtRemote.sh
 
-${ROWAN_PROJECTS_HOME}/gt4gemstone/scripts/build-gt4gemstone-release.sh
+${ROWAN_PROJECTS_HOME}/gt4gemstone/scripts/release/package-release.sh
 
-chmod +x ${ROWAN_PROJECTS_HOME}/gt4gemstone-${GTOOLKIT_EXPECTED_VERSION}/inputRelease.sh
-${ROWAN_PROJECTS_HOME}/gt4gemstone-${GTOOLKIT_EXPECTED_VERSION}/inputRelease.sh
+chmod +x ${GEMSTONE_WORKSPACE}/${RELEASED_PACKAGE_GEMSTONE_NAME}/inputRelease.sh
+${GEMSTONE_WORKSPACE}/${RELEASED_PACKAGE_GEMSTONE_NAME}/inputRelease.sh
 
 exit 0
