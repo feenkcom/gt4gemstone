024cecba61dbfd30c2a375f52b6088b328dcc0ef
Alistair Grant <alistairgrant@fastmail.com.au>
2025-01-04 08:25:20 +0100

    Use 3.7.1.4 on feenk CI

--- scripts/jenkins_preconfigure_gemstone.sh
+++ scripts/jenkins_preconfigure_gemstone.sh
@@ -19,22 +19,25 @@ cd ..
 ROOT_DIR=`pwd`
 echo "ROOT_DIR=${ROOT_DIR}"
 echo "WORKSPACE=${WORKSPACE}"
+echo "GEMSTONE_WORKSPACE=${GEMSTONE_WORKSPACE}"
+echo "GT4GEMSTONE_VERSION=${GT4GEMSTONE_VERSION}"
+echo "RELEASED_PACKAGE_GEMSTONE_NAME="${RELEASED_PACKAGE_GEMSTONE_NAME}"
 
 cd $WORKSPACE/..
-echo "Download GemStone archives if required"
-if [ ! -e GemStone64Bit3.7.1-x86_64.Linux.zip ]
+echo "Download GemStone archives if required in $(pwd)"
+if [ ! -e GemStone64Bit3.7.1.4-x86_64.Linux.zip ]
 then
 	echo "Preconfigure: Downloading GS to `pwd`"
-	wget http://downloads.gemtalksystems.com/pub/GemStone64/3.7.1/GemStone64Bit3.7.1-x86_64.Linux.zip
+	wget http://downloads.gemtalksystems.com/pub/GemStone64/3.7.1.4/GemStone64Bit3.7.1.4-x86_64.Linux.zip
 fi
 
-if [ ! -e GemStoneClientLibs3.7.1-x86_64.Linux.zip ]
+if [ ! -e GemStoneClientLibs3.7.1.4-x86_64.Linux.zip ]
 then
 	echo "Preconfigure: Downloading GS Libs to `pwd`"
-	wget http://downloads.gemtalksystems.com/pub/GemStone64/3.7.1/GemStoneClientLibs3.7.1-x86_64.Linux.zip
+	wget http://downloads.gemtalksystems.com/pub/GemStone64/3.7.1.4/GemStoneClientLibs3.7.1.4-x86_64.Linux.zip
 fi
 
 cd ${imageDirectory}
-echo "Link to GemStone archives"
-ln -s $WORKSPACE/../GemStone64Bit3.7.1-x86_64.Linux.zip
-ln -s $WORKSPACE/../GemStoneClientLibs3.7.1-x86_64.Linux.zip
+echo "Link to GemStone archives in $(pwd)"
+ln -s $WORKSPACE/../GemStone64Bit3.7.1.4-x86_64.Linux.zip
+ln -s $WORKSPACE/../GemStoneClientLibs3.7.1.4-x86_64.Linux.zip
